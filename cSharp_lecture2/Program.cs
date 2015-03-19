4dacedc80fd0f1502a4e9bc84a1f336585f67bdb
Xuchen1591 <magina.1591@outlook.com>
2015-03-19 13:36:17 +0800

    Array

---
+++ cSharp_lecture2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cSharp_lecture2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			/*
+			//class "Array" functions
+			int[] a = {1,2,4,2,455,6,2,4,6,2,9,0 };
+
+			Array.Sort(a);
+			Array.Reverse(a);
+
+			foreach (int i in a)
+				Console.WriteLine(i);
+			//Array ends here
+			 * */
+
+
+		}
+	}
+}
